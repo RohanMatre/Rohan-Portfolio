bf923d808d3a650618ca988f25b1c35cd4c231ef
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2023-07-08 06:49:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// import React from 'react' ---> new versions of it we don't require it.
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+
+ReactDOM.render(<App/>,document.querySelector("#root"))
+
